71527df5b8e36ce74c872732301e1db4040af50f
Melchor Cerezo <melcerezo@gmail.com>
2019-09-19 12:58:04 -0700

    created example-work.js, fixed return statement in example-work.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Loaded react-dom");
-
-console.log("Webpack works");
+const myWork = [
+  {
+    'title': "Richer Picture Website",
+    'image': {
+      'desc': "Richer Picture",
+      'src': "images/richer_picture.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Richer Picture Webinar Archive",
+    'image': {
+      'desc': "Richer Picture webinar_archive",
+      'src': "images/webinar_archive.png",
+      'comment': ""
+    }
+  }
+]
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
