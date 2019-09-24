ec0c970320d7d2e138737a2f0ffc0d6f05c906a3
Melchor Cerezo <melcerezo@gmail.com>
2019-09-24 09:24:14 -0700

    added dietselect link and picture

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ const portLinks = [
 const myWork = [
   {
     'title': "Richer Picture Website",
-    'href': "https://example.com",
+    'href': "https://www.richerpicture.com/",
     'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "Richer Picture",
@@ -37,7 +37,7 @@ const myWork = [
   },
   {
     'title': "Richer Picture Webinar Archive",
-    'href': "https://example.com",
+    'href': "https://www.richerpicture.com/webinar-login.php",
     'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "Richer Picture webinar_archive",
@@ -47,11 +47,21 @@ const myWork = [
   },
   {
     'title': "Python Tweetbot",
-    'href': "https://example.com",
+    'href': "https://github.com/melcerezo/Tweetbot",
     'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "Python Tweetbot",
       'src': "images/TweetBot.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "DietSelect",
+    'href': "https://github.com/melcerezo/dietselect",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'image': {
+      'desc': "DietSelect",
+      'src': "images/DietSelect.png",
       'comment': ""
     }
   }
