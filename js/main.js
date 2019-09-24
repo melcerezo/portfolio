88ed0651c9095181700264b6e809c1ffef4262da
Melchor Cerezo <melcerezo@gmail.com>
2019-09-24 00:09:57 -0700

    added tweetbot project to examples

--- js/main.js
+++ js/main.js
@@ -44,6 +44,16 @@ const myWork = [
       'src': "images/webinar_archive.png",
       'comment': ""
     }
+  },
+  {
+    'title': "Python Tweetbot",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'image': {
+      'desc': "Python Tweetbot",
+      'src': "images/TweetBot.png",
+      'comment': ""
+    }
   }
 ]
 ReactDOM.render(<WorkHeader myLinks={portLinks}/>, document.getElementById('example-header'));
